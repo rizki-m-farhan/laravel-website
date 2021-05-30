3ab4863e29fc7fb67f66b7f8f547d25a2b06be58
rizki-m-farhan <rizkifarhan7539@gmail.com>
2021-05-30 23:18:03 +0700

    menambahkan file laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1><br />
+    <p> 
+    Room Study adalah tempat situs blog website untuk belajar yang membahas teknologi pendidikan internet, khususnya bagi pemula umum yang siapa saja tertarik.
+
+Room Study ini memberikan tutorial Teknologi Layanan Pendidikan Internet : Android, Aplikasi, Bisnis, Belajar Coding, Cyber Security, File Dokumen, Kesehatan, Live Streaming, Order Online, Pendidikan, Room Study, Seputar Informasi, Sistem Operasi, Teknologi.
+
+Terima Kasih Telah berkunjung di blog disitus website kami Jangan lupa like, follow & share di media sosial. Semoga ilmu yang saya berikan ini bermanfaat :)
+    </p>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
